a316f697891e210914586cf11713aa3741ca13fd
s.savin <s.savin@plagate.ru>
2024-06-24 15:37:55 +0300

    Add scripts: commit with date, start app flask, start many apps flask

---
+++ start_apps.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+~/AppData/Local/Programs/Python/Python312/python.exe ~/flask-mysql-project/app.py &
+~/AppData/Local/Programs/Python/Python312/python.exe /p/s.savin/flask-mysql-agp/app.py &
+~/AppData/Local/Programs/Python/Python312/python.exe /p/s.savin/flask-project-full/app.py &
