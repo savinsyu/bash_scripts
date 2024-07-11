1a9c11d2de3357cde1b0e211b660809c70d7d794
s.savin <s.savin@plagate.ru>
2024-07-11 15:00:10 +0300

    Add commit date: Thu Jul 11 15:00:09 RTZ 2024

--- pull.sh
+++ pull.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 # Переходим в папку mysql-flask-project
-echo "Принимаем изменения со всех репозиториев!" 
-cd ~/mysql-flask-project &
+cd ~/mysql-flask-project &&
 # Принимаем изменения с репозитория
-git pull &
+git pull &&
 # Переходим в папку flask-project-full
-cd ~/flask-project-full &
+cd ~/flask-project-full &&
 # Принимаем изменения с репозитория
-git pull &
+git pull &&
 # Переходим в папку bash_scripts
-cd ~/bash_scripts &
+cd ~/bash_scripts &&
 # Принимаем изменения с репозитория
-git pull &
+git pull &&
 # Переходим в папку python_scripts
-cd ~/python_scripts &
+cd ~/python_scripts &&
 # Принимаем изменения с репозитория
-git pull &
+git pull &&
 # Переходим в папку ml-advanced
-cd ~/ml-advanced &
+cd ~/ml-advanced &&
 # Принимаем изменения с репозитория
-git pull &
+git pull &&
 # Переходим в папку draft
-cd ~/draft &
+cd ~/draft &&
 # Принимаем изменения с репозитория
 git pull
